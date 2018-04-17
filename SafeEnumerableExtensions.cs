83f9d5216a98598f05425f5a84ba8e8a0a085bbd
Roman Ustinov <romanustinov9@gmail.com>
2018-04-18 01:19:33 +0300

    Add project files.

---
+++ SafeEnumerableExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SafeEnumerable
+{
+    public static class SafeEnumerableExtensions
+    {
+        public static bool MoveNextSafely<T>(this IEnumerator<T> enumerator)
+        {
+            try
+            {
+                return enumerator.MoveNext();
+            }
+            catch (Exception e)
+            {
+                if (e is InvalidOperationException)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+        }
+
+        public static IEnumerable<T> Safe<T>(this IEnumerable<T> enumerable)
+        {
+            var enumerator = enumerable.GetEnumerator();
+
+            bool end = enumerator.MoveNextSafely();
+
+            while (!end)
+            {
+                yield return enumerator.Current;
+                end = enumerator.MoveNextSafely();
+            }
+        }
+    }
+}
